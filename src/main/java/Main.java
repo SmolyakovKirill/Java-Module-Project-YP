c4f0a30601a3093ae6d50af9020d8e15e8ba3413
SmolyakovKirill <smolyakov.kirusha@gmail.com>
2023-10-22 16:16:10 +0600

    add under control scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        int members = scanner.nextInt();
+
+        if(members <= 1) {
+            System.out.println("Ошибка");
+            return;
+        }
+
+        System.out.println(members);
     }
 }
