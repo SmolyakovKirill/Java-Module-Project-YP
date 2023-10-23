247d33727f3525728352835ad164aa4175b30337
SmolyakovKirill <smolyakov.kirusha@gmail.com>
2023-10-23 11:25:20 +0600

    add under control exception for input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int members = 0;
 
-        int members = scanner.nextInt();
+        try {
+            members = scanner.nextInt();
+        }
 
+        catch (Exception ex){
+            System.out.println("Необходимо ввести целочисленную переменнную");
+            return;
+        }
         if(members <= 1) {
             System.out.println("Ошибка");
             return;
